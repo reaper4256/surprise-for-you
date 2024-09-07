5b8dc1b4ed14cb74d26790d71868c3b721dfa5bd
reaper4256 <94520859+reaper4256@users.noreply.github.com>
2024-09-07 23:30:36 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const audio =
+
+docamettbodyrySdbeatent:audyo');
+
+audio.addEventListener('play', () => { body.classList.add('play-music'); });
+
+audio.addEventListener('pause', () => { body.classList.remove('play-music'); });
